9c83e775310973d832fa6c051b0ecb2e6f387858
kushwahaarzoo <146642196+kushwahaarzoo@users.noreply.github.com>
2026-02-01 22:28:10 +0530

    Add files via upload

---
+++ lab1/input.java
@@ -0,0 +1,19 @@
+/**
+	 * This is a documentation comment (Javadoc).
+ * It can span multiple lines and is used by the 'javadoc' tool
+		 * to generate API documentation.
+ */
+public class CommentExample {
+
+	// This is a single-line comment.
+	public static void main(String[] args) {
+		// This line prints a message to the console.
+		System.out.println("Hello, World!");
+
+		/* This is a multi-line comment.
+		   It is typically used to comment out
+		   blocks of code or for detailed explanations
+		   that are not meant for Javadoc. */
+	}
+}
+
